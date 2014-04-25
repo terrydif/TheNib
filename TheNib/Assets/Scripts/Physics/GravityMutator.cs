6e828e5c9d269e27d3df8d8f1023eeed1b21a868
Ryan <raxteran@gmail.com>
2014-04-25 12:46:41 -0400

    GravityMutator

--- TheNib/Assets/Scripts/Physics/GravityMutator.cs
+++ TheNib/Assets/Scripts/Physics/GravityMutator.cs
@@ -2,14 +2,38 @@
 using System.Collections;
 
 public class GravityMutator : MonoBehaviour {
+	
+	public Vector3 currentGravity;
+	public float transitionSpeed;
 
-	// Use this for initialization
-	void Start () {
+	private Vector3 originalGravity;
 	
+	void Awake() 
+	{
+		originalGravity = Physics.gravity;
+		currentGravity = originalGravity;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void updateGravity(Vector3 newGravity)
+	{
+		Physics.gravity = originalGravity;
+		currentGravity = originalGravity;
+	}
+
+	public void updateGravity(Vector3 newGravity, float speed)
+	{
+		Physics.gravity = newGravity;
+		currentGravity = newGravity;
+		transitionSpeed = speed;
+	}
+
+	public void resetGravity()
+	{
+		updateGravity(originalGravity);
+	}
+
+	void Update() 
+	{
+		Physics.gravity = Vector3.Lerp(Physics.gravity, currentGravity, Time.deltaTime * transitionSpeed);
 	}
 }
