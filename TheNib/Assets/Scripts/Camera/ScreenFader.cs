2acc337019c2a0fa4dc21c1ecc7457c1f1d162e1
Ryan <raxteran@gmail.com>
2014-04-29 14:04:49 -0400

    Timeline and CameraFades

---
+++ TheNib/Assets/Scripts/Camera/ScreenFader.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenFader : MonoBehaviour
+{
+	public bool fadingIn;
+
+	private float timer;
+	private Color previousColor;
+	private Color nextColor;
+
+	private Color clearColor;
+	private Color whiteColor;
+
+	private float fadeSpeed = 1;
+	
+	void Start ()
+	{
+		clearColor = new Color (1, 1, 1, 0);
+		whiteColor = new Color (1, 1, 1, 1);
+
+		// Set Initial Colors
+		nextColor = clearColor;
+		SetColor(whiteColor);
+	}
+	
+	void Update ()
+	{
+		if (fadingIn) 
+		{
+			timer = Mathf.Clamp (timer + Time.deltaTime * fadeSpeed, 0, 1);
+			renderer.material.color = Color.Lerp (previousColor, nextColor, timer);
+		}
+	}
+	
+	public void FadeIn ()
+	{
+		fadingIn = true;
+	}
+
+	public void SetColor(Color color)
+	{
+		previousColor = nextColor;
+		nextColor = color;
+	}
+}
---
+++ TheNib/Assets/Scripts/Managers/TimelineManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimelineManager : MonoBehaviour {
+	
+	private float time;
+	private int currentRoom;
+
+	public RoomOne roomOne;
+
+	void Start() 
+	{
+		currentRoom = 1;
+		time = 0.0f;
+	}
+
+	void Update() 
+	{
+		time += Time.deltaTime;
+	}
+
+	public void transitionRooms()
+	{
+		switch (currentRoom)
+		{
+		case 1:
+			Debug.Log("Switch to Room 2!");
+			roomOne.exitRoom();
+			currentRoom = 2;
+			break;
+
+		case 2:
+			Debug.Log("Switch to Room 3!");
+			currentRoom = 3;
+			break;
+
+		case 3:
+			Debug.Log("Kill the Player!");
+			currentRoom = 0;
+			break;
+
+		default:
+			Debug.Log("Don't Do Anything!");
+			break;
+		}
+	}
+}
