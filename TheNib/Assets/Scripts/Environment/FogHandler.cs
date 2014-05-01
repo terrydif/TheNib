05de40bad944c6df8f1ee68bbfbc3097ba9d81f5
Ryan <raxteran@gmail.com>
2014-05-01 11:10:15 -0400

    Never gonne give you up, never gonna let you down

--- TheNib/Assets/Scripts/Environment/FogHandler.cs
+++ TheNib/Assets/Scripts/Environment/FogHandler.cs
@@ -7,20 +7,26 @@ public class FogHandler : MonoBehaviour {
 	public float speed;
 	public float colorSpeed;
 
-	private float density;
-	private Color color;
+	private float _density;
+	private Color _color;
 
 	void Awake () 
 	{
-		density = RenderSettings.fogDensity;
-		color = RenderSettings.fogColor;
+		_density = 0.01f;
+		_color = new Color(0.7f,0.0f,1.0f,0.6f);
 	}
 
 	void Update () 
 	{
-		RenderSettings.fogDensity = Mathf.MoveTowards(RenderSettings.fogDensity, 0.01f, Time.deltaTime * speed);
-		RenderSettings.fogColor = Color.Lerp(RenderSettings.fogColor, new Color(0.8f,0.1f,1.0f,0.3f), Time.deltaTime * colorSpeed);
-		riftCamera.BackgroundColor = Color.Lerp(riftCamera.BackgroundColor, new Color(0.8f,0.1f,1.0f,0.3f), Time.deltaTime * colorSpeed);
+		RenderSettings.fogDensity = Mathf.MoveTowards(RenderSettings.fogDensity, _density, Time.deltaTime * speed);
+		RenderSettings.fogColor = Color.Lerp(RenderSettings.fogColor, _color, Time.deltaTime * colorSpeed);
+		riftCamera.BackgroundColor = Color.Lerp(riftCamera.BackgroundColor, _color, Time.deltaTime * colorSpeed);
+	}
+
+	public void setFog (float density, Color color)
+	{
+		_density = density;
+		_color = color;
 	}
 	
 }
