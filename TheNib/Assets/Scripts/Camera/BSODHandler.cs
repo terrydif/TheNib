cada12544bd1383f073080b4925fa0b875d8cfcd
Ryan <raxteran@gmail.com>
2014-05-02 12:12:00 -0400

    BSOD

---
+++ TheNib/Assets/Scripts/Camera/BSODHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BSODHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () 
+	{
+		Invoke("turnOn", 2.5f);
+	}
+
+	void turnOn()
+	{
+		renderer.enabled = true;
+	}
+}
